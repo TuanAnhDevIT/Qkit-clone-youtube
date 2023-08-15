51d5671fb99a73fb0a93c7d24af21161aa480fd4
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-08-15 16:22:30 +0700

    ádmnád

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/Qkit-clone-youtube/",
+  base: "/Qkit-clone-youtube/",
   plugins: [react()],
   server: {
     port: 3000,
