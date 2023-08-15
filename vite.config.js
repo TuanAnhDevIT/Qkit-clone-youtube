5dd9d91fce827432edd51972a719e5bbae9344b6
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-08-15 15:53:15 +0700

    vjp

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://tuananhdevit.github.io/Qkit-clone-youtube/",
+  base: "/Qkit-clone-youtube/",
   plugins: [react()],
   server: {
     port: 3000,
