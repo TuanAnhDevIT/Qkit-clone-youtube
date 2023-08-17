4da8107b054e39323de78961c36e6bc74a88e2e5
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-08-17 10:19:34 +0700

    done config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dns from 'dns'
+import { error } from 'console'
 // https://vitejs.dev/config/server-options.html#server-options
 dns.setDefaultResultOrder('verbatim')
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.BASE_URL,
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-})
+// export default defineConfig({
+//   base: process.env.BASE_URL,
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+// })
+export default defineConfig((config) => {
+  const env = loadEnv(config.mode, process.cwd(), "")
+  return {
+    base: env.BASE_URL,
+    plugins: [react()],
+    server: {
+      port: 3000,
+    }
+  }
+}) 
